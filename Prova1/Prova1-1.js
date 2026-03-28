7151744410fc1400bc6e41ff65f59a77db88ff0f
Thom <Thomasaulinskialbeche@gmail.com>
2026-03-27 21:26:53 -0300

    Add files via upload

---
+++ Prova1/Prova1-1.js
@@ -0,0 +1,86 @@
+//aqui esta as categorias dos filtros
+const ordenar = []
+const entregaGratis = []
+const valeRefeicao = []
+const distancia = []
+const entregaParceira = []
+const superRestaurantes = []
+const filtros = []
+//aqui decidi por outras categorias, talves possam ser vistas em ordenar ou filtro, mas para melhor modelagem de cupom vou deixar separadas pelo comentario
+const clube = []
+const valePresente = [] 
+// uma ideia de como seria a parte de limpar
+function limpar(opcoesFiltro){
+    opcoesFiltro.lenght = 0
+}
+const cupom = [
+    {
+        porPorcentagem:false,
+        porcentagem: 0,
+        porValor: true,
+        valorDescontoReais: 20,
+        tipoDeDesconto: [valeRefeicao],
+
+
+    }
+]
+const lojas =[
+    {
+        logoRestaurante: "link",
+        nomeRestaurante: "nome do restaurante",
+        temSuperRestaurante: false,
+        nota: 5.0,
+        categoria: "Marmitex",
+        distanciaKM: 3.5 ,
+        tempoEsperaMinMinutos: 30,
+        tempoEsperaMaxMinutos: 60,
+        temFretegratis: false,
+        valorFreteReais: 12.40,
+        temCupom: true,
+        cupom: [{porPorcentagem:true,porcentagem: 10, tipoDeDesconto:[clube]}]
+    },
+        {
+        logoRestaurante: "link",
+        nomeRestaurante: "Supa",
+        temSuperRestaurante: true,
+        nota: 3.6,
+        categoria: "Marmitex",
+        distanciaKM: 3.5 ,
+        tempoEsperaMinMinutos: 30,
+        tempoEsperaMaxMinutos: 60,
+        temFretegratis: false,
+        valorFreteReais: 10.40,
+        temCupom: true,
+        cupom: [{porPorcentagem:true,porcentagem: 10, tipoDeDesconto:[clube]}]
+
+    },
+        {
+        logoRestaurante: "link",
+        nomeRestaurante: "entraSim",
+        temSuperRestaurante: false,
+        nota: 3.0,
+        categoria: "Marmitex",
+        distanciaKM: 3.5 ,
+        tempoEsperaMinMinutos: 50,
+        tempoEsperaMaxMinutos: 60,
+        temFretegratis: true,
+        valorFreteReais: 12.40,
+        temCupom: true,
+        cupom: [{porPorcentagem:true,porcentagem: 10, tipoDeDesconto:[clube]}]
+
+    }
+]
+const porEntregasGratis = lojas.filter(loja => loja.temFretegratis == true)
+const porSuperRestaurante = lojas.filter(loja => loja.temSuperRestaurante == true)
+const porTempoMin = lojas.filter(loja => loja.tempoEsperaMinMinutos <= 30)
+
+function promoMap(lojas){
+    return lojas
+    .filter(loja => loja.nota >= 4)
+    .map(loja => {
+    return loja.valorFreteReais - 5
+    });
+}
+    
+
+console.log(promoMap(lojas))
---
+++ Prova1/Prova1-2.js
@@ -0,0 +1,60 @@
+const endereco = {
+    // logradouro (rua/avenida), número, complemento (apto/bloco), bairro, cidade, estado e CEP.
+    logradouro: "Avenida henry ford",
+    numero:511,
+    complemento: "parque da mooca",
+    cidade: "sao paulo",
+    estado: "SP",
+    cep: "03109-001",
+    pais: "Brasil"
+}
+
+const anuncio = {
+    titulo: "Cascada",
+    feitopor: "rebo",
+    quando: ["quarta 3 de junho", 22, 6],
+    local: "komplexo tempo",
+    endereco,
+    tickets:{
+        tituloTicket: "promo 4",
+        descricao: "",
+        precoReais: 130,
+        quantidade: 0 
+    },
+    temInteresse: false,
+    interesados:[710],
+    posterPromocional: "link"
+}
+
+const tickets = [
+    {
+        tituloTicket: "promo 4",
+        descricao: "",
+        precoReais: 130,
+        quantidade: 0 
+    },
+        {
+        tituloTicket: "promo 4",
+        descricao: "",
+        precoReais: 150,
+        quantidade: 0 
+    },
+        {
+        tituloTicket: "promo 4",
+        descricao: "lorem la é isso dai mesmo",
+        precoReais: 240,
+        quantidade: 0 
+    }
+]
+
+function menorCupom(tickets) {
+    let menor = tickets[0]; 
+    tickets.forEach(ticket => {
+        if (ticket < menor) {
+            menor = ticket; 
+        }
+    });
+    return menor;
+}
+
+console.log(menorCupom(tickets))
